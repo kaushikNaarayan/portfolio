606307fc80739ed517406e9fb8566ef48d6793cb
Kaushik Naarayan <kaushiknaarayan@Kaushiks-MacBook-Pro.local>
2025-02-21 20:12:48 +0530

    copy edits, and visual clean ups

--- scripts.js
+++ scripts.js
@@ -8,6 +8,19 @@ fetch('config.json')
   });
 
 document.addEventListener('DOMContentLoaded', function() {
+  const collapsibles = document.querySelectorAll('.collapsible');
+  collapsibles.forEach(collapsible => {
+    collapsible.addEventListener('click', function() {
+      this.classList.toggle('active');
+      const content = this.nextElementSibling;
+      if (content.style.display === 'block') {
+        content.style.display = 'none';
+      } else {
+        content.style.display = 'block';
+      }
+    });
+  });
+
   const links = document.querySelectorAll('nav ul li a');
 
   for (const link of links) {
